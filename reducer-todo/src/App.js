3daf8a6b8743b53916bcdf1f4826cecdc6db3ca7
gebhartn <nicholas.gebhart@gmail.com>
2019-10-08 19:51:10 -0700

    skeleton app complete

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Form from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
 
 export default () => {
   return (
     <>
       <Form />
+      <ToDoList />
     </>
   );
 };
---
+++ reducer-todo/src/components/ToDoList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <main>
+      <h1>Return Item here</h1>
+    </main>
+  );
+};
